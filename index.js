ee48d41f50107a50428492187648decad358bc2a
Olga_Belaya <protenot@yandex.ru>
2023-10-05 17:55:18 +0300

    add hw-1

---
+++ index.js
@@ -0,0 +1,30 @@
+const tree = {
+    "name": 1,
+
+    "items": 
+    [{
+      "name": 2,
+        "items":
+         [{ "name": 3 },
+          { "name": 4 }]
+    }, 
+    {
+       "name": 5,
+        "items": 
+        [{ "name": 6 }]
+    }]
+    }
+    function createTree(tree, prefix = '') {
+        console.log(prefix + tree.name);
+     
+        if (Array.isArray(tree.items)) {
+          const lengthArray = tree.items.length;
+         
+          for (let i = 0; i < lengthArray; i++) {
+            
+            const childPrefix = prefix + ('├── ');
+            createTree(tree.items[i], childPrefix);
+          }
+        }
+      }
+      createTree(tree)

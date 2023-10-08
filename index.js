9ce01963b245551ae12f799a939e72ba3288247b
Olga_Belaya <protenot@yandex.ru>
2023-10-08 17:12:12 +0300

    correct again

--- index.js
+++ index.js
@@ -14,17 +14,22 @@ const tree = {
         [{ "name": 6 }]
     }]
     }
-    function createTree(tree, prefix = '') {
-        console.log(prefix + tree.name);
-     
-        if (Array.isArray(tree.items)) {
-          const lengthArray = tree.items.length;
-         
-          for (let i = 0; i < lengthArray; i++) {
-            
-            const childPrefix = prefix + (i === lengthArray - 1 ? '└── ' : '├── ');
-            createTree(tree.items[i], childPrefix);
-          }
+    function createTree(tree, prefix = '', isLast = true, isRoot = false) {
+      if (!isRoot) {
+        console.log(prefix + (isLast ? '└── ' : '├── ') + tree.name);
+      } else {
+        console.log(tree.name);
+      }
+    
+      if (Array.isArray(tree.items)) {
+        const lengthArray = tree.items.length;
+    
+        for (let i = 0; i < lengthArray; i++) {
+          const childPrefix = prefix + (isLast ? ' ' : '│ ');
+          const isLastChild = i === lengthArray - 1;
+          createTree(tree.items[i], childPrefix, isLastChild);
         }
       }
-      createTree(tree)
+    }
+    
+    createTree(tree, '', true, true);
